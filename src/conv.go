6bc2549963071fbfbf8ecafd954597b2d5fb848b
Lucas Tétreault <lucastetreault@gmail.com>
2019-04-01 23:19:18 -0600

    latest

--- src/conv.go
+++ src/conv.go
@@ -52,18 +52,18 @@ func (hdl Handle) Conversation(_msgs ...Message) ([]string, error) {
 		return nil, errors.New("Must pass at least one Message.")
 	}
 
-	msg := make([]C.struct_pam_message, len(_msgs))
-	resp := make([]C.struct_pam_response, len(_msgs))
+	msg := make([]*C.struct_pam_message, len(_msgs))
+	resp := make([]*C.struct_pam_response, len(_msgs))
 
 	for _, _msg := range _msgs {
 		msgStruct := C.struct_pam_message{msg_style: C.int(_msg.Style), msg: C.CString(_msg.Msg)}
 		defer C.free(unsafe.Pointer(msgStruct.msg))
 
-		msg = append(msg, msgStruct)
-		resp = append(resp, C.struct_pam_response{})
+		msg = append(msg, &msgStruct)
+		resp = append(resp, &C.struct_pam_response{})
 	}
 
-	code := C.do_conv(hdl.ptr(), C.int(len(_msgs)), &msg[0], &resp[0])
+	code := C.do_conv(hdl.ptr(), C.int(len(_msgs)), &msg, &resp)
 	if code != C.PAM_SUCCESS {
 		return nil, fmt.Errorf("Got non-success from the function: %d", code)
 	}
